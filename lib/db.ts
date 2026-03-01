88e33a3fddd9de229ae85b452557df6b30447168
Guilherme <guilherme.salviano12@outlook.com>
2026-03-01 18:19:18 -0300

    refactor: update connection type

--- lib/db.ts
+++ lib/db.ts
@@ -19,12 +19,12 @@ const devType: DataSourceOptions = {
 };
 
 const prodType: DataSourceOptions = {
-  type: "postgres",
+  type: "mariadb",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 3306,
+  database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
   synchronize: false,
   logging: true,
 }
