1a7cefc65c9c9ab74f824ef23f35a85281ee8333
Guilherme <guilherme.salviano12@outlook.com>
2026-03-01 15:58:36 -0300

    refactor(db): add prod version in database connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,20 +1,42 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
-import { User } from "../entities/User";
-import { Weather } from "@/entities/Weather";
-import { WeatherHour } from "@/entities/WeatherHour";
+import { DataSource, DataSourceOptions } from "typeorm";
+import * as dotenv from "dotenv";
 import { Todo } from "@/entities/Todo";
 import { TodoRecurrence } from "@/entities/TodoRecurrence";
+import { User } from "@/entities/User";
+import { Weather } from "@/entities/Weather";
+import { WeatherHour } from "@/entities/WeatherHour";
 
-export const AppDataSource = new DataSource({
-    type: "sqlite",
-    database: "database.db", 
-    synchronize: true,
-    logging: true,
-    entities: [User, Weather, WeatherHour, Todo, TodoRecurrence],
-    subscribers: [],
-    migrations: [],
-});
+dotenv.config({});
+
+const isDevMode = process.env.NODE_ENV === "development";
+
+const devType: DataSourceOptions = {
+  type: "sqlite",
+  database: "database.db",
+  synchronize: true,
+  logging: true,
+};
+
+const prodType: DataSourceOptions = {
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  logging: true,
+}
+
+const devOrProdDataSource = {
+  ...(isDevMode ? devType : prodType),
+  entities: [Todo, TodoRecurrence, User, Weather, WeatherHour],
+  subscribers: [],
+  migrations: ["database/*.ts"],
+};
+
+export const AppDataSource = new DataSource(devOrProdDataSource);
 
 export const getDatabaseConnection = async () => {
   if (!AppDataSource.isInitialized) {
