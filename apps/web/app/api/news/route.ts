c39bc2e8a33b17b05cbaf539629479e9aaac6d40
Guilherme <guilherme.salviano12@outlook.com>
2026-03-16 17:00:15 -0300

    revert(news): api news priority

--- apps/web/app/api/news/route.ts
+++ apps/web/app/api/news/route.ts
@@ -4,43 +4,40 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
-    const googleNewsData = await fetchGoogleNewsAPI();
-
-    if (googleNewsData) {
-      const { news_results, menu_links } = googleNewsData;
-
-      const news = news_results
-        .map((article, index) => {
-          const currentMenu = menu_links[index];
-          const isLocalNews = currentMenu?.title === "Suas notícias locais";
-
-          return {
-            id: index,
-            source: article.source?.name ?? null,
-            title: article.title,
-            tag: isLocalNews ? menu_links[index - 1]?.title : currentMenu?.title,
-            url: article.link,
-          };
-        })
-        .filter((_, index) => menu_links[index]?.title !== "Suas notícias locais");
-
-      return NextResponse.json(
-        { message: "News data retrieved successfully from serpapi", data: news },
-        { status: 200 }
-      );
-    }
-
     const mediastackData = await fetchMediastackAPI();
 
-    const news = mediastackData.data.map((article, index) => ({
-      id: index,
-      source: article.source,
-      title: article.title,
-      tag: article.category,
-      url: article.url,
-    }));
+    if (mediastackData) {
+      const news = mediastackData.data.map((article, index) => ({
+        id: index,
+        source: article.source,
+        title: article.title,
+        tag: article.category,
+        url: article.url,
+      }));
 
-    return NextResponse.json({ message: "News data retrieved successfully from mediastack", data: news }, { status: 200 });
+      return NextResponse.json({ message: "News data retrieved successfully from mediastack", data: news }, { status: 200 });
+    }
+
+    const googleNewsData = await fetchGoogleNewsAPI();
+
+    const { news_results, menu_links } = googleNewsData;
+
+    const news = news_results
+      .map((article, index) => {
+        const currentMenu = menu_links[index];
+        const isLocalNews = currentMenu?.title === "Suas notícias locais";
+
+        return {
+          id: index,
+          source: article.source?.name ?? null,
+          title: article.title,
+          tag: isLocalNews ? menu_links[index - 1]?.title : currentMenu?.title,
+          url: article.link,
+        };
+      })
+      .filter((_, index) => menu_links[index]?.title !== "Suas notícias locais");
+
+    return NextResponse.json({ message: "News data retrieved successfully from serpapi", data: news }, { status: 200 });
   } catch (error: unknown) {
     console.error(error);
     return NextResponse.json({ error: "Failed to retrieve news data" }, { status: 500 });
