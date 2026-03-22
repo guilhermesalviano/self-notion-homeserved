6cb8803687bcfd172dd0ffa538df348ba43384ae
Guilherme <guilherme.salviano12@outlook.com>
2026-03-22 12:15:55 -0300

    chore: add next config images

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'casaos.local',
+        port: '3001',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
